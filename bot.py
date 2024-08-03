62be3a870d934ccaf7ad28f13ddd45d333e07f8d
MirossOffYT <74865590+MirossOFFYT@users.noreply.github.com>
2024-08-03 19:55:24 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,10 +15,31 @@ intents.message_content = True
 client = discord.Client(intents=intents)
 
 def send_webhook(url, username, avatar_url, content):
+    # Format correct de la signature pour éviter les embeds
+    signature = "-# sent from [{}](<{}>)".format(username, url)
     data = {
         "username": username,
         "avatar_url": avatar_url,
-        "content": content
+        "content": "{}\n\n{}".format(content, signature)
+    }
+    result = requests.post(url, json=data)
+    try:
+        result.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        print(err)
+
+def send_file_webhook(url, username, avatar_url, file_url, content):
+    # Format correct de la signature pour éviter les embeds
+    signature = "# sent from [{}]({})".format(username, url)
+    data = {
+        "username": username,
+        "avatar_url": avatar_url,
+        "content": "{}\n\n{}".format(content, signature),
+        "embeds": [{
+            "image": {
+                "url": file_url
+            }
+        }]
     }
     result = requests.post(url, json=data)
     try:
@@ -35,9 +56,19 @@ async def on_message(message):
     if message.author.bot:
         return
 
+    target_webhook_url = None
     if message.channel.id == CHANNEL_ID_SERVER_1:
-        send_webhook(WEBHOOK_URL_SERVER_2, message.author.display_name, str(message.author.avatar.url), message.content)
+        target_webhook_url = WEBHOOK_URL_SERVER_2
     elif message.channel.id == CHANNEL_ID_SERVER_2:
-        send_webhook(WEBHOOK_URL_SERVER_1, message.author.display_name, str(message.author.avatar.url), message.content)
+        target_webhook_url = WEBHOOK_URL_SERVER_1
+
+    if target_webhook_url:
+        username = message.author.display_name
+        avatar_url = str(message.author.avatar.url)
+        if message.attachments:
+            for attachment in message.attachments:
+                send_file_webhook(target_webhook_url, username, avatar_url, attachment.url, message.content)
+        else:
+            send_webhook(target_webhook_url, username, avatar_url, message.content)
 
 client.run(BOT_TOKEN)
